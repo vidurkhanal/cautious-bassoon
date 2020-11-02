52f915520f85e94b71fe6ff60f92e1f8eb90d71b
Vidur <vidurkhanal@outlook.com>
2020-11-02 16:12:49 +0545

    INITIALIZING GIT REPO

---
+++ src/resolvers/post.ts
@@ -0,0 +1,52 @@
+import { Post } from "../entities/Post";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
+import { MyContext } from "../types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const postToUpdate = await em.findOne(Post, { id });
+    if (!postToUpdate) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      postToUpdate.title = title;
+      await em.persistAndFlush(postToUpdate);
+    }
+    return postToUpdate;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
+  }
+}
---
+++ src/resolvers/user.ts
@@ -0,0 +1,119 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Provided Username is Too Short",
+          },
+        ],
+      };
+    }
+    if (options.username.length <= 5) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Provided Password is Too Short",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505" || err.detail.includes("already exists")) {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "Username has already been taken.",
+            },
+          ],
+        };
+      }
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em,req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username Not Found.",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password Is Incorrect",
+          },
+        ],
+      };
+    }
+    req.session.userId = user.id;
+
+    return { user };
+  }
+}
